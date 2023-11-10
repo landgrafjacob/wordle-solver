b4565420d181433f793bc504443fdcaa12031c00
Jake Landgraf <landgrafjacob@gmail.com>
2023-11-10 17:06:07 -0500

    Add website bucket

---
+++ s3_website_bucket.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "wordle-solver-website"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_object" "website" {
+  bucket = aws_s3_bucket.website.id
+  for_each = fileset("${path.module}/website", "**")
+  key = each.value
+  source = "${path.module}/website/${each.value}"
+  source_hash = filemd5("${path.module}/website/${each.value}")
+}
