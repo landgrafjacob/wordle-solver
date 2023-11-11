d8f092c7837cd6ca4f0583e89c9cad3729987ed4
Jake Landgraf <landgrafjacob@gmail.com>
2023-11-11 03:34:46 -0500

    More fixes

--- api_gateway.tf
+++ api_gateway.tf
@@ -40,12 +40,6 @@ resource "aws_api_gateway_rest_api" "wordle_solver" {
   name = "wordle-solver"
 }
 
-#resource "aws_api_gateway_resource" "root" {
-#  rest_api_id = aws_api_gateway_rest_api.wordle_solver.id
-#  parent_id   = aws_api_gateway_rest_api.wordle_solver.root_resource_id
-#  path_part   = ""
-#}
-
 resource "aws_api_gateway_method" "root" {
   rest_api_id = aws_api_gateway_rest_api.wordle_solver.id
   resource_id = aws_api_gateway_rest_api.wordle_solver.root_resource_id
@@ -60,5 +54,6 @@ resource "aws_api_gateway_integration" "root" {
   type        = "AWS"
   uri         = "arn:aws:apigateway:${data.aws_region.current.name}:s3:path/${aws_s3_bucket.website.bucket}/index.html"
   credentials = aws_iam_role.api_gateway.arn
+  integration_http_method = aws_api_gateway_method.root.http_method
 }
 
