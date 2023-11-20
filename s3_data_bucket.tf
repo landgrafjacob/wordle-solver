465a83835f2e4bea8bfb29f7fdff4e512b76ac0d
Jake Landgraf <landgrafjacob@gmail.com>
2023-11-19 20:15:19 -0500

    Add etag to s3 objects

--- s3_data_bucket.tf
+++ s3_data_bucket.tf
@@ -6,10 +6,12 @@ resource "aws_s3_object" "wordlist" {
   bucket = aws_s3_bucket.data_bucket.id
   key    = "wordlist.txt"
   source = "data/wordlist.txt"
+  etag = "data/wordlist.txt"
 }
 
 resource "aws_s3_object" "letter_frequencies" {
   bucket = aws_s3_bucket.data_bucket.id
   key    = "letter-frequency.txt"
   source = "data/word_freq_with_pos.csv"
+  etag   = "data/word_freq_with_pos.csv"
 }
